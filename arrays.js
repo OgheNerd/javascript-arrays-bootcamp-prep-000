cdba4cce75ba4cc987dd7d22a88b78d7260c83c0
Oghene Michael <zonalmeet@gmail.com>
2018-01-30 19:20:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  const newArray = [element, ...array];
+  newArray
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+}
+
+
+function addElementToEndOfArray(array, element) {
+  const newArray = [...array, element];
+  newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  array[0];
+}
