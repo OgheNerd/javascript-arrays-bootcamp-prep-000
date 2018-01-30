06a1e283b0cf735f396044b2bb31ad4f0f411361
Oghene Michael <zonalmeet@gmail.com>
2018-01-30 19:35:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  const newArray = array.slice(1);
+  return newArray;
+
 }
